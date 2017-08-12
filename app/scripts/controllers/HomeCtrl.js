fae0001936a34570a8d6933193ef7fecfa2a975b
Edward Graham <Edward@Edwards-MBP.fios-router.home>
2017-08-11 23:31:57 -0400

    finished site basic features and add old school styling

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,5 +1,5 @@
 (function(){
-  function HomeCtrl($uibModal, $scope, Room, Message) {
+  function HomeCtrl($cookies, $uibModal, $scope, Room, Message) {
     $scope.rooms = Room.all;
     $scope.activeRoom = null;
     this.openModal = function(){
@@ -14,8 +14,20 @@
       $scope.activeRoom.messages = Message.getByRoomId(chatRoom.$id);
     };
 
+    this.sendMessage = function(newMessage) {
+      var currentUser = $cookies.get('blocChatCurrentUser');
+      var currentMessage = {
+        roomId: $scope.activeRoom.$id,
+        content: newMessage,
+        username: currentUser,
+        sentAt: new Date(),
+      };
+      Message.send(currentMessage);
+      $scope.newMessage = null;
+    };
+
   }
   angular
     .module('blocChat')
-    .controller('HomeCtrl', ['$uibModal','$scope','Room', 'Message', HomeCtrl]);
+    .controller('HomeCtrl', ['$cookies','$uibModal','$scope','Room', 'Message', HomeCtrl]);
 })();
